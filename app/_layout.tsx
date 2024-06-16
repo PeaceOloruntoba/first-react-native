144d672bd03cb3207e90ef031f772ad61b192d0f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-06-16 18:13:41 +0100

    changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { Slot } from 'expo-router'
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <Slot />
   )
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({})
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
-export default function Index() {
+export default function index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Aora!</Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
