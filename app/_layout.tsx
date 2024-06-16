14d01b1aa12ae791034e1dfad0599afabb499a55
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-06-16 18:25:16 +0100

    routing multiple screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { Slot } from 'expo-router'
+import { Slot, Stack } from 'expo-router'
 const RootLayout = () => {
   return (
-    <Slot />
+    <Stack>
+      <Stack.Screen name='index' options={{headerShown: false}} />
+    </Stack>
   )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
 export default function index() {
   return (
     <View style={styles.container}>
       <Text>Aora!</Text>
       <StatusBar style='auto' />
+      <Link href={"/profile"} style={{color:'blue'}}>Go to profile</Link>
     </View>
   )
 }
