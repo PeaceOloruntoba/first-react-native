109b1703bc35647b2073bec1dbec7a4bbb71ec93
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-06-17 06:33:47 +0100

    sddfghj

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { ScrollView, View, Image, Text, StatusBar } from "react-native";
-import { Link } from "expo-router";
+import { ScrollView, View, Image, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { images } from "../constants";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/CustomButton";
 
 export default function index() {
   return (
@@ -12,10 +13,36 @@ export default function index() {
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
-            resizeMethod="contain"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover endless possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            expolaration with Aura
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
