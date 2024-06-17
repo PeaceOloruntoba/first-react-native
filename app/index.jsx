2ff1a1737644b8d8b4c391b704d735b4cc10708b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-06-17 05:38:50 +0100

    Good morning Github

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>
         where it all began
