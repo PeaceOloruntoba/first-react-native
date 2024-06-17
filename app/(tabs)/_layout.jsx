9468864433fe083aabc30ddf5d95b927403dc3b9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-06-17 06:10:49 +0100

    dfgfh

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,10 +1,100 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-export default function Tab() {
+import { View, Text, Image } from "react-native";
+import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
+const TabIcon = ({ icons, color, name, focused }) => {
   return (
-    <View>
-      <Text>Tab</Text>
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icons}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`} style={{color: color}}
+      >
+        {name}
+      </Text>
     </View>
-  )
+  );
+};
+export default function TabsLayout() {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#ffa001",
+          tabBarInactiveTintColor: "#cdcde0",
+          tabBarStyle: {
+              backgroundColor: "#161622",
+              borderTopWidth: 1,
+              borderTopColor: '#232533',
+              height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icons={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icons={icons.bookmark}
+                color={color}
+                name="Bookmark"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icons={icons.plus}
+                color={color}
+                name="Create"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icons={icons.profile}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
-import { Link } from 'expo-router';
+import React from "react";
+import { ScrollView, View, Image, Text, StatusBar } from "react-native";
+import { Link } from "expo-router";
+import { images } from "../constants";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue' }}>
-        where it all began
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMethod="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
